d30d2c716380bd95f72940c07e42ae988847c726
Nikita <0forthey0@gmail.com>
2024-04-05 12:59:18 +0300

    Add worker schemas

---
+++ source/schemas/workers.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class WorkerAdd(BaseModel):
+    telegram_id: int
+
+    name: str
+    surname: str
+    patronymic: str | None
+
+    access_right: str
+
+
+class Worker(WorkerAdd):
+    id: int
