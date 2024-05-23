6d46b78162e117d3803262611e0b39d1cb27bbf3
Nikita <0forthey0@gmail.com>
2024-05-23 21:13:51 +0300

    test change

--- source/bot/utility/get_id_by_username.py
+++ source/bot/utility/get_id_by_username.py
@@ -10,7 +10,8 @@ async def get_user_id(username) -> int | None:
             settings.get_bot_username,
             settings.get_tg_api_id,
             settings.get_tg_api_hash) as client:
-        bot = await client.start(bot_token=settings.TOKEN)
+        # bot = await client.start(bot_token=settings.TOKEN)
+        bot = await client.start('0')
 
         async with bot:
             try:
