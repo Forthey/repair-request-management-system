00cd31a7e6840620612097ebcba3ec1870da00fa
Nikita <0forthey0@gmail.com>
2024-05-24 02:08:28 +0300

    Set auto response decode in redis

--- source/redis_db/workers.py
+++ source/redis_db/workers.py
@@ -8,7 +8,7 @@ from schemas.workers import Worker
 
 async def load_workers():
     redis: Redis
-    async with Redis(connection_pool=pool) as redis:
+    async with Redis(connection_pool=pool, decode_responses=True) as redis:
         workers: list[Worker] = await get_workers()
 
         pipe = redis.pipeline()
@@ -20,7 +20,7 @@ async def load_workers():
 
 async def reload_worker(telegram_id: int):
     redis: Redis
-    async with Redis(connection_pool=pool) as redis:
+    async with Redis(connection_pool=pool, decode_responses=True) as redis:
         worker: Worker = await get_worker(telegram_id, True)
         if not worker.active:
             await redis.delete(f"user:{telegram_id}")
@@ -30,11 +30,11 @@ async def reload_worker(telegram_id: int):
 
 async def check_common_access(telegram_id: int) -> bool:
     redis_client: Redis
-    async with Redis(connection_pool=pool) as redis_client:
+    async with Redis(connection_pool=pool, decode_responses=True) as redis_client:
         return await redis_client.exists(f"user:{telegram_id}")
 
 
 async def check_admin_rights(telegram_id: int) -> bool:
     redis_client: Redis
-    async with Redis(connection_pool=pool) as redis_client:
-        return (await redis_client.get(f"user:{telegram_id}")).decode("utf-8") == "Админ"
+    async with Redis(connection_pool=pool, decode_responses=True) as redis_client:
+        return (await redis_client.get(f"user:{telegram_id}")) == "Админ"
