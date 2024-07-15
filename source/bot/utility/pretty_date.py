4b2b28ae7fefc11b28e0c26d53ad538c662b09ee
Drekalov Nikita <0forthey0@gmail.com>
2024-07-15 18:35:51 +0300

    Fix time display, now it matches UTC+3

--- source/bot/utility/pretty_date.py
+++ source/bot/utility/pretty_date.py
@@ -1,5 +1,6 @@
 from datetime import datetime
+from pytz import timezone
 
 
 def date_to_str(date: datetime) -> str:
-    return date.strftime("%d.%m.%Y %H:%M")
+    return date.astimezone(timezone("Europe/Moscow")).strftime("%d.%m.%Y %H:%M")
